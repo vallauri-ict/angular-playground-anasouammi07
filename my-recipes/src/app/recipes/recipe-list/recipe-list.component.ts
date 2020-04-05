741d2250b530c882c059b90843660b8665e9860a
anasouammi07 <58808560+anasouammi07@users.noreply.github.com>
2020-04-05 15:49:03 +0200

    Seconda lezione

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe-model';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes:Recipe[]=[
+    new Recipe("A test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg%22"),
+    new Recipe("Second test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg%22"),
+    new Recipe("Third test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg%22")
+  ]
 
   constructor() { }
 
